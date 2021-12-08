dc5d23f86ce4a7a5e3b2d705e0985a49a22d303e
Will Hawkins <32672557+WSJHawkins@users.noreply.github.com>
2021-12-08 22:21:56 +0000

    Add files via upload

---
+++ ingenious2.py
@@ -0,0 +1,364 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 31 14:01:04 2019
+
+@author: Will
+"""
+import ingeniousHelper
+import numpy as np
+import math
+import random
+import colorama
+colorama.init(strip=False)
+ 
+    
+#Decides what computer maximises
+def scoreFunction(mode,score,maxScore,scoreBoard,otherScoreBoard,turnNo,randomVars):
+    value = False
+    
+    #naive
+    if(mode == 0):
+        if(score[0] > maxScore[0]):
+            value = True
+                   
+    #considers min then total score if same
+    elif(mode == 1):
+        minScore = min(score[1])
+        minScore2 = min(maxScore[1])
+        if(minScore > minScore2):
+            value = True
+        elif(minScore == minScore2):
+            if(score[0] > maxScore[0]):
+                value = True
+           
+    #uses a weighting system based on raising the lowest pieces
+    elif(mode == 2):
+        copyOfScore = score[1].copy()
+        copyOfScore.sort()
+        copyOfMaxScore = maxScore[1].copy()
+        copyOfMaxScore.sort()
+        
+        totalScore = 0
+        multiplyer = 10
+        for s in copyOfScore:
+            totalScore = totalScore + multiplyer * s
+            multiplyer = multiplyer - 1
+            
+        totalScore2 = 0
+        multiplyer = 10
+        for s in copyOfMaxScore:
+            totalScore2 = totalScore2 + multiplyer * s
+            multiplyer = multiplyer - 1
+            
+        if(totalScore > totalScore2):
+            value = True
+    
+    #uses a comparisson system
+    elif(mode == 3):
+        copyOfScore = score[1].copy()
+        copyOfScore.sort()
+        copyOfMaxScore = maxScore[1].copy()
+        copyOfMaxScore.sort()
+
+        total = 0
+        for i in range(0,6):
+            total = total + ( (copyOfScore[i] - copyOfMaxScore[i]) * 1/(i+1) )
+            
+        
+        if(total > 0):
+            value = True
+            
+     #Pierre
+    elif(mode == 4):
+        copyOfScore = score[1].copy()
+        copyOfScore.sort()
+        copyOfMaxScore = maxScore[1].copy()
+        copyOfMaxScore.sort()
+        
+        totalScore = 0
+        for s in copyOfScore:
+            if(s > 13):
+                totalScore = totalScore + s/3
+            elif(13 >= s > 11):
+                totalScore = totalScore + s*0.8
+            else:
+                totalScore = totalScore + s
+            
+        maxScoreTotal = 0
+        for s in copyOfMaxScore:
+            if(s > 13):
+                maxScoreTotal = maxScoreTotal + s/3
+            elif(13>= s >11):
+                maxScoreTotal = maxScoreTotal +s*0.8
+            else:
+                maxScoreTotal = maxScoreTotal + s
+         
+        if(totalScore > maxScoreTotal):
+            value = True
+
+    #Straight up maximisation
+    elif(mode == 5):
+        scoreCopy = score[1].copy()
+        maxScoreCopy = maxScore[1].copy()
+        
+        if(scoreCopy > maxScoreCopy):
+            value = True
+
+    #same as 3 but compares to scoreboard
+    elif(mode == 6):
+        copyOfScore = score[1].copy()
+        copyOfScore.sort()
+        copyOfMaxScore = maxScore[1].copy()
+        copyOfMaxScore.sort()
+
+        total = 0
+        for i in range(0,6):
+            total = total + ( (copyOfScore[i] - scoreBoard[i]) * 1/(i+1) )
+            
+        maxScoreTotal = 0
+        for i in range(0,6):
+            maxScoreTotal = maxScoreTotal + ( (copyOfMaxScore[i] - scoreBoard[i]) * 1/(i+1) )
+        if(total > maxScoreTotal):
+            value = True
+            
+    
+    #Takes turnNo into account, low turn number do 0 high turn number do 6
+    elif(mode == 7):
+        copyOfScore = score[1].copy()
+        copyOfScore.sort()
+        copyOfMaxScore = maxScore[1].copy()
+        copyOfMaxScore.sort()
+    
+        total = 0
+        for i in range(0,6):
+            total = total + ( (copyOfScore[i] - scoreBoard[i]) * 1/(3*i+1) )            
+        total = score[0] * (1/(0.5*turnNo+1)) + total * (1-1/(0.5*turnNo+1))
+            
+        maxScoreTotal = 0
+        for i in range(0,6):
+            maxScoreTotal = maxScoreTotal + ( (copyOfMaxScore[i] - scoreBoard[i]) * 1/(2*i+1))
+        maxScoreTotal = maxScore[0] * (1/(0.5*turnNo+1)) + maxScoreTotal * (1-1/(0.5*turnNo+1))
+            
+        if(total > maxScoreTotal):
+            value = True
+            
+    #Piers2
+    elif(mode==8):
+        copyOfScore = score[1].copy()
+        copyOfScore.sort()
+        copyOfMaxScore = maxScore[1].copy()
+        copyOfMaxScore.sort()
+        difference=[0,0,0,0,0,0]
+        difference1=[0,0,0,0,0,0]
+        t1=0
+        t2=0
+        a = 0.4
+        b = 0.01
+        for i in range(0,6):
+            difference[i] = copyOfScore[i] - scoreBoard[i]
+            difference1[i] = copyOfMaxScore[i] - scoreBoard[i]
+        
+        totalScore = 0
+        for i in range(0,6):
+            if(copyOfScore[i]<=10):
+                totalScore = totalScore + difference[i]
+            elif(copyOfScore[i] <= 12):
+                if(scoreBoard[i]<=10):
+                    t1 = scoreBoard[i] + difference[i] - 10
+                    t2 = 10 - scoreBoard[i]
+                else:
+                    t1 = 12 - scoreBoard[i]
+                    t2 = 0
+                totalScore = totalScore + t2 + t1*a    
+            else:
+                if(scoreBoard[i]<10):
+                    totalScore = (10-scoreBoard[i])+ 2*a + (copyOfScore[i] - 12)*b +totalScore
+                elif(scoreBoard[i] < 13):
+                    totalScore = (12 - scoreBoard[i])*a + (copyOfScore[i] - 12)*b + totalScore
+                else:
+                    totalScore = totalScore + difference[i]*b
+            
+        maxScoreTotal = 0
+        for i in range(0,6):
+             if(copyOfMaxScore[i]<=10):
+                maxScoreTotal = maxScoreTotal + difference1[i]
+             elif(copyOfMaxScore[i] <= 12):
+                if(scoreBoard[i]<=10):
+                    t1 = scoreBoard[i] + difference1[i] - 10
+                    t2 = 10 - scoreBoard[i]
+                else:
+                    t1 = 12 - scoreBoard[i]
+                    t2 = 0
+                maxScoreTotal = maxScoreTotal + t2 + t1*a    
+             else:
+                if(scoreBoard[i]<10):
+                    maxScoreTotal = (10-scoreBoard[i])+ 2*a + (copyOfMaxScore[i] - 12)*b + maxScoreTotal
+                elif(scoreBoard[i] < 13):
+                    maxScoreTotal = (12 - scoreBoard[i])*a + (copyOfMaxScore[i] - 12)*b + maxScoreTotal
+                else:
+                    maxScoreTotal = maxScoreTotal + difference1[i]*b       
+            
+        if(totalScore > maxScoreTotal):
+            value = True 
+            
+            
+    #Makes decisions after looking at oponents points - optimised weights          
+    elif(mode == 9):
+        alpha = randomVars[0]
+        beta = randomVars[1]
+        alpha2 = randomVars[2]
+        beta2 = randomVars[3]
+        alpha3 = randomVars[4]
+        beta3 = randomVars[5]
+        weight1 = (1/(alpha*turnNo+beta))
+        weight2 = randomVars[6]
+        weight3 = randomVars[7]
+        
+    
+        copyOfScore = score[1].copy()
+        copyOfMaxScore = maxScore[1].copy()
+        copyOfScoreBoard = scoreBoard.copy()
+        copyOfScoreBoard.sort()
+                        
+        #sum up scores
+        totalScore = score[0] * weight1
+        totalScore2 = maxScore[0] * weight1
+        
+         #Always take the double go
+        for i in range(0,6):
+            if(score[1][i] == 18 and scoreBoard[i] != 18):
+                totalScore = totalScore + 999
+            if(maxScore[1][i] == 18 and scoreBoard[i] != 18):
+                totalScore2 = totalScore2 + 999
+                
+        for i in range(0,6):
+            val = copyOfScoreBoard.index(scoreBoard[i])
+            totalScore = totalScore + weight2 * ( (copyOfScore[i] - scoreBoard[i]) * (1/(alpha2*val+beta2)) )
+            totalScore = totalScore + weight3 * (copyOfScore[i]* (otherScoreBoard[i] - scoreBoard[i]) * (1/(alpha3*val+beta3)))
+            totalScore2 = totalScore2 + weight2 * ( (copyOfMaxScore[i] - scoreBoard[i]) * (1/(alpha2*val+beta2)) )
+            totalScore2 = totalScore2 + weight3 * (copyOfMaxScore[i]* (otherScoreBoard[i] - scoreBoard[i]) * (1/(alpha3*val+beta3)) )
+         
+        if(totalScore > totalScore2):
+            value = True
+        
+        #Means it always picks something
+        if(maxScore[0] == 0):
+            value = True
+    
+    return value
+
+
+bestVars = [0,0,0,0,0,0,0,0]
+bestPercent = 0
+for z in range(0,10000):
+    winner = [0,0]
+    numOfGames = 50
+    viewGame = False
+    gameOver = 0
+    var1 = random.randint(0,100)/10 
+    var2 = random.randint(0,100)/10 
+    var3 = random.randint(0,100)/10 
+    var4 = random.randint(0,100)/10 
+    var5 = random.randint(0,100)/10 
+    var6 = random.randint(0,100)/10 
+    var7 = random.randint(0,100)/10 
+    var8 = random.randint(0,100)/10 
+    randomVars = [var1,var2,var3,var4,var5,var6,var7,var8]
+    for k in range(0,8):
+        if(randomVars[k] == 0):
+            randomVars[k] = 0.01
+    for n in range(numOfGames):
+        #Initialise Game
+        numOfPlayers = 2 
+        board = createBoard(numOfPlayers)
+        
+        #Initialise Tile Bag
+        tileBag = fillTileBag()
+        
+        #Player 1
+        scoreBoard = [0,0,0,0,0,0]
+        tileRack,tileBag = generateTileRack(tileBag)
+    
+        #Player2
+        scoreBoard2 = [0,0,0,0,0,0]
+        tileRack2,tileBag = generateTileRack(tileBag)
+        #Print Game State
+        if(viewGame):
+            printGameState(board,tileRack,scoreBoard,2)
+               
+        #Start Game
+        for i in range(0,1000):
+            if(viewGame):
+                input('Press ENTER to continue...')
+            
+            #Player1
+            anotherGo = 1
+            while(anotherGo == 1):
+                board,scoreBoard,tileRack,tileBag,gameOver,anotherGo = playTurnComputer(board,scoreBoard,scoreBoard2,tileRack,tileBag,i,9,randomVars)
+                if(viewGame and gameOver == 0):
+                    print("Player 1")
+                    printGameState(board,tileRack,scoreBoard,1)
+            if(gameOver==1):
+                break
+            
+            if(viewGame):
+                input('Press ENTER to continue...')
+            
+            #Player2
+            anotherGo = 1
+            while(anotherGo == 1):
+                board,scoreBoard2,tileRack2,tileBag,gameOver,anotherGo = playTurnComputer(board,scoreBoard2,scoreBoard,tileRack2,tileBag,i,0,randomVars)     
+                #board,scoreBoard2,tileRack2,tileBag,gameOver,anotherGo = playTurnHuman(board,scoreBoard2,scoreBoard,tileRack2,tileBag,i,numOfPlayers,8,randomVars)     
+                if(viewGame and gameOver == 0):
+                    print("Player 2")
+                    printGameState(board,tileRack2,scoreBoard2,1)
+            if(gameOver==1):
+                break   
+           
+        if(viewGame): 
+            print("Game Over")
+            printGameState(board,tileRack,scoreBoard,1)
+            printGameState(board,tileRack2,scoreBoard2,3)
+        
+        #Who Won   
+        scoreBoard.sort()
+        scoreBoard2.sort()
+        if(scoreBoard>scoreBoard2):
+            winner[0] = winner[0] + 1
+            #print("Player 1 Won Game: "+str(n)+ " so far: "+str(winner[0]))
+        else:
+            winner[1] = winner[1] + 1
+            #print("Player 2 Won Game: "+str(n)+ " so far: "+str(winner[1]))
+    
+    percentage = winner[0]/numOfGames * 100
+    percentage = round(percentage,1)
+    print("Player 1 Won "+str(percentage)+"% Of The "+str(numOfGames)+" Games")
+    if(percentage >= 70):
+        bestVars[0] = var1
+        bestVars[1] = var2
+        bestVars[2] = var3
+        bestVars[3] = var4
+        bestVars[4] = var5
+        bestVars[5] = var6
+        bestVars[6] = var7
+        bestVars[7] = var8
+        print(percentage)
+        print(bestVars[0])
+        print(bestVars[1])
+        print(bestVars[2])
+        print(bestVars[3])
+        print(bestVars[4])
+        print(bestVars[5])
+        print(bestVars[6])
+        print(bestVars[7])
+        print()
+
+print(bestVars[0])
+print(bestVars[1])
+print(bestVars[2])
+print(bestVars[3])
+print(bestVars[4])
+print(bestVars[5])
+print(bestVars[6])
+print(bestVars[7])
+    
---
+++ ingeniousHelper.py
@@ -0,0 +1,389 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  2 09:56:13 2019
+
+@author: Will
+"""
+
+def numToColour(x):
+    returnVal = ""
+    if(x == 1):
+        returnVal = colorama.Fore.RED + colorama.Style.BRIGHT
+    elif(x == 2):
+        returnVal = colorama.Fore.GREEN + colorama.Style.BRIGHT
+    elif(x == 3):
+        returnVal = colorama.Fore.MAGENTA + colorama.Style.BRIGHT
+    elif(x == 4):
+        returnVal = colorama.Fore.BLUE + colorama.Style.BRIGHT
+    elif(x == 5):
+        returnVal = colorama.Fore.YELLOW + colorama.Style.BRIGHT
+    elif(x == 6):
+        returnVal = '\033[33m' 
+    else:
+        returnVal = colorama.Fore.BLACK
+      
+    return returnVal
+ 
+def createBoard(numOfPlayers):
+    
+    board = np.zeros((15,15), dtype=int)   
+    maxWidth = 15 - (4-numOfPlayers)*2
+    for j in range(15):
+        width = j + 2 * numOfPlayers
+        if(width > (maxWidth)):
+            width = (maxWidth*2) - width
+        for i in range(15):
+            if( j < (4-numOfPlayers) or j > (15-(4-numOfPlayers)-1)):
+                board[j][i] = -1           
+            elif(i < (4-numOfPlayers) or i > (15-(4-numOfPlayers)-1)  ):
+                    board[j][i] = -1
+            elif(i > (((4-numOfPlayers)+width-1))):
+                    board[j][i] = -1
+         
+    board[2][2] = 1
+    board[2][7] = 2
+    board[7][2] = 3
+    board[7][12] = 4
+    board[12][2] = 5
+    board[12][7] = 6
+    return board
+
+       
+def printBoard(board):
+    for j in range(15):
+        print()
+        count = 0
+        
+        for i in range(15):
+            if(board[j][i]==-1):
+                count = count + 1
+                
+        for k in range(math.floor(count/2)):
+            print(" .", end = '')
+            if(k != math.floor(count/2)-1):
+                print(" ", end = '')
+            if ((j % 2) != 1) and (k == math.floor(count/2)-1):
+                print(" ", end = '')
+            
+        for i in range(15):
+            if(board[j][i] != -1):
+                print(" ", end = '')
+                print(numToColour(board[j][i]) + str(board[j][i]) + colorama.Style.RESET_ALL, end = '')
+                print(" ", end = '')
+                
+        for k in range(math.ceil(count/2)):
+            print(" . " , end = '')
+    print()
+
+def fillTileBag():
+    tileBag = []
+    #5 of each double
+    for i  in range(1,7):
+        for j in range(0,5):
+            tileBag.append([i,i])
+            
+    #6 of each pair 
+    for i in range(1,6):
+        for j in range(i+1,7):
+            if(i!=j):
+                for k in range(0,6):
+                    tileBag.append([i,j])
+    return tileBag
+
+
+            
+def printTileRack(tileRack):
+    print(tileRack[0][0],"  ",tileRack[1][0],"  ",tileRack[2][0],"  ",tileRack[3][0],"  ",tileRack[4][0],"  ",tileRack[5][0])
+    print(tileRack[0][1],"  ",tileRack[1][1],"  ",tileRack[2][1],"  ",tileRack[3][1],"  ",tileRack[4][1],"  ",tileRack[5][1])
+     
+def printScoreBoard(scoreBoard):
+    print()
+    print("---------------------------------------------------------------")
+    print ("|   0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 |")
+    for j in range(1,7):
+        print('|' + numToColour(j) + str(j)+'  ', end = '')
+        for i in range(scoreBoard[j-1]):
+            print('.  ', end = '')
+        print('x  ', end = '')
+        for i in range(18 - scoreBoard[j-1]):
+            print('.  ', end = '')
+        print(colorama.Style.RESET_ALL + '|')
+    print("---------------------------------------------------------------")
+    print()
+    
+def printGameState(board,tileRack,scoreBoard,method):
+    if(method == 0):
+        printBoard(board)
+        printScoreBoard(scoreBoard)
+        printTileRack(tileRack)
+    if(method == 1):
+        printBoard(board)
+        printScoreBoard(scoreBoard)
+    if(method == 2):
+        printBoard(board)
+    if(method == 3):
+        printScoreBoard(scoreBoard)
+
+def availablePlaces(j,i,board):
+    if(j < 7):
+        placeList = [[j+1,i+1],[j,i+1],[j+1,i],[j-1,i],[j,i-1],[j-1,i-1]]
+    elif(j==7):
+        placeList = [[j+1,i-1],[j,i+1],[j+1,i],[j-1,i],[j,i-1],[j-1,i-1]]
+    elif(j > 7):
+        placeList = [[j+1,i-1],[j,i+1],[j+1,i],[j-1,i],[j,i-1],[j-1,i+1]]
+    if(board[j][i] != 0):
+        placeList = []
+    placeList2 = []
+    for x in placeList:
+        if(x[0] >= 0 and x[1] >= 0 and x[0] < 15 and x[1] < 15):
+            if(board[x[0]][x[1]] == 0):
+                placeList2.append(x)
+    return placeList2
+
+def nextDoorPieces(j,i,board):
+    if(j < 7):
+        placeList = [[j+1,i+1],[j,i+1],[j+1,i],[j-1,i],[j,i-1],[j-1,i-1]]
+    elif(j==7):
+        placeList = [[j+1,i-1],[j,i+1],[j+1,i],[j-1,i],[j,i-1],[j-1,i-1]]
+    elif(j>7):
+        placeList = [[j+1,i-1],[j,i+1],[j+1,i],[j-1,i],[j,i-1],[j-1,i+1]]
+    if(board[j][i] != 0):
+        placeList = []
+    placeList2 = []
+    for x in placeList:
+        if(x[0] >= 0 and x[1] >= 0 and x[0] < 15 and x[1] < 15):
+            if(board[x[0]][x[1]] > 0):
+                placeList2.append(x)
+    return placeList2
+    
+def generateTileRack(tileBag):
+    tileRack = []
+    for i in range(0,6):
+        tileNo = random.randint(0,len(tileBag)-1)
+        tileRack.append(tileBag[tileNo])
+        tileBag.remove(tileBag[tileNo])
+    return tileRack,tileBag
+
+def refreshTileRack(tileRack,tileBag):
+    for i in tileRack:
+        if(i[0] == -1):              
+            tileNo = random.randint(0,len(tileBag)-1)
+            i[0] = tileBag[tileNo][0]
+            i[1] = tileBag[tileNo][1]
+            tileBag.remove(tileBag[tileNo])
+    return tileRack,tileBag
+
+def incrementScore(score,tileNum):
+    if(score[1][tileNum] < 18):
+        score[0] = score[0] + 1
+        score[1][tileNum] = score[1][tileNum] + 1
+    return score
+    
+    
+def scoreLine(board,location,n,score):
+    if(location[0] == n[0]):
+        #going side to side
+        tmp = location[1]
+        location[1] = n[1]
+        n[1] = n[1]+n[1]-tmp
+        if(n[0] >= 0 and n[1] >= 0 and n[0] < 15 and n[1] < 15):
+            if(board[location[0]][location[1]] == board[n[0]][n[1]]):
+                score = incrementScore(score,board[n[0]][n[1]]-1)
+#                score[0] = score[0] + 1
+#                score[1][board[n[0]][n[1]]-1] = score[1][board[n[0]][n[1]]-1] + 1
+                score = scoreLine(board,location,n,score)
+    else:
+        #diaganol
+        incrementI = n[1]-location[1]
+        if(n[0]==7):
+            if(incrementI == 0):
+                incrementI = 1
+            else:
+                incrementI = 0
+        location[1] = n[1]
+        n[1] = n[1] + incrementI
+        tmp = location[0]
+        location[0] = n[0]
+        n[0] = n[0]+n[0]-tmp
+        if(n[0] >= 0 and n[1] >= 0 and n[0] < 15 and n[1] < 15):
+            if(board[location[0]][location[1]] == board[n[0]][n[1]]):
+                score = incrementScore(score,board[n[0]][n[1]]-1)
+#                score[0] = score[0] + 1   
+#                score[1][board[n[0]][n[1]]-1] = score[1][board[n[0]][n[1]]-1] + 1
+                score = scoreLine(board,location,n,score)
+        
+    return score
+    
+    
+    
+def scoreMove(location,position,tile,board,scoreBoard):
+    total = 0
+    for score in scoreBoard:
+        total = total + score
+    score = [total,[scoreBoard[0],scoreBoard[1],scoreBoard[2],scoreBoard[3],scoreBoard[4],scoreBoard[5]]]
+    
+    for n in nextDoorPieces(location[0],location[1],board):
+        if (n[0] != position[0] or n[1] != position[1]):
+            if(tile[0] == board[n[0]][n[1]]):
+                score = incrementScore(score,tile[0]-1)
+#                score[0] = score[0]+1
+#                score[1][tile[0]-1] = score[1][tile[0]-1] + 1
+                #then look at line 
+                score = scoreLine(board,location[:],n[:],score[:])
+                               
+    for n in nextDoorPieces(position[0],position[1],board):
+        if (n[0] != location[0] or n[1] != location[1]):
+            if(tile[1] == board[n[0]][n[1]]):
+                score = incrementScore(score,tile[1]-1)
+#                score[0] = score[0] + 1
+#                score[1][tile[1]-1] = score[1][tile[1]-1] + 1
+                #then look at line
+                score = scoreLine(board,position[:],n[:],score[:])
+    return score
+    
+def setScoreBoard(maxScore,scoreBoard):
+    scoreBoard = maxScore[:]
+    return scoreBoard
+    
+def removeTileFromRack(tileRack,tile):
+    for t in tileRack:
+        if(t[0] == tile[0] and t[1] == tile[1]):
+            t[0] = -1
+            t[1] = -1
+            break
+    return tileRack
+
+def placeTile(board,bestLocation):
+    board[bestLocation[0][0]][bestLocation[0][1]] = bestLocation[2][0]
+    board[bestLocation[1][0]][bestLocation[1][1]] = bestLocation[2][1]
+    return board
+
+def playTurnComputer(board,scoreBoard,otherScoreBoard,tileRack,tileBag,turnNo,method,randomVars):
+    gameOver = 1
+    anotherGo = 0   
+    bestLocation = []
+    score = [0,[0,0,0,0,0,0]]
+    maxScore = [0,[0,0,0,0,0,0]]
+    
+    
+    for j in range(15):
+        for i in range(15):
+            location = [j,i]
+            if(board[location[0]][location[1]] == 0):
+                positions = availablePlaces(location[0],location[1],board)
+                if(len(positions) > 0):
+                    gameOver = 0
+                    
+    if(gameOver == 0):             
+        for j in range(15):
+            for i in range(15):
+                location = [j,i]
+                if(board[location[0]][location[1]] == 0):
+                    positions = availablePlaces(location[0],location[1],board)
+                    for p in positions:
+                        for t in tileRack:
+                            score = [0,[0,0,0,0,0,0]]
+                            score = scoreMove(location,p,t,board,scoreBoard)
+                            if(scoreFunction(method,score,maxScore,scoreBoard,otherScoreBoard,turnNo,randomVars)):
+                                maxScore = score
+                                bestLocation = [location,p,t]             
+        if(len(bestLocation) != 0):
+            for i in range(0,6):
+                if(maxScore[1][i] == 18 and scoreBoard[i] != 18):
+                    anotherGo = 1
+            scoreBoard = setScoreBoard(maxScore[1],scoreBoard)
+            board = placeTile(board,bestLocation)
+            tileRack = removeTileFromRack(tileRack,bestLocation[2])
+            tileRack,tileBag = refreshTileRack(tileRack,tileBag)
+        else:
+            #gameOver = 1
+            print("Computer couldn't choose a move?")
+            printGameState(board,tileRack,scoreBoard,0)
+            printGameState(board,tileRack,otherScoreBoard,3)
+            print(turnNo)
+            print(maxScore)
+            print(bestLocation)
+            print(score)
+    
+    return board,scoreBoard,tileRack,tileBag,gameOver,anotherGo
+
+def playTurnHuman(board,scoreBoard,otherScoreBoard,tileRack,tileBag,turnNo,numOfPlayers,method,randomVars):
+    gameOver = 1
+    bestLocation = []
+    score = [0,[0,0,0,0,0,0]]
+    maxScore = [0,[0,0,0,0,0,0]]
+    
+    for j in range(15):
+        for i in range(15):
+            location = [j,i]
+            if(board[location[0]][location[1]] == 0):
+                positions = availablePlaces(location[0],location[1],board)
+                if(len(positions) > 0):
+                    gameOver = 0
+                    
+    if(gameOver == 0):  
+        printTileRack(tileRack)
+        tile = [0,0]
+        while tile not in tileRack:
+            tile = input("Enter Tile Choice: ") 
+            tile = tile.split()
+            tile[0] = int(tile[0])
+            tile[1] = int(tile[1])
+              
+        
+        error = 1
+        position = [0,0]
+        while (error == 1 or board[location[0]][location[1]] != 0 or board[position[0]][position[1]] != 0 ):
+            error = 0
+            location = input("Enter your value:") 
+            location = location.split(",")
+            if (len(location) == 2):
+                position = location[1]
+                location = location[0]
+                location = location.split(" ")
+                if (len(location) == 2):
+                    location[0] = int(location[0]) + 4-numOfPlayers -1
+                    location[1] = int(location[1]) + 4-numOfPlayers -1
+                else:
+                    error = 1
+                    print("input error")
+                    
+                position = position.split(" ")
+                if (len(position) == 2):
+                    position[0] = int(position[0]) + 4-numOfPlayers -1
+                    position[1] = int(position[1]) + 4-numOfPlayers -1
+                else:
+                    error = 1
+                    print("input error")
+            else:
+                error = 1
+                print("input error")
+            if (error == 0):
+                value = availablePlaces(location[0],location[1],board) 
+                if(position not in value):
+                    error = 1
+                    print("not valid move")
+    #                print(board)
+    #                print(location)
+    #                print(position)
+                    print(value)
+        
+        score = [0,[0,0,0,0,0,0]]
+        score = scoreMove(location,position,tile,board,scoreBoard)
+        maxScore = score
+        bestLocation = [location,position,tile]
+                                
+        anotherGo = 0                
+        for i in range(0,6):
+            if(maxScore[1][i] == 18 and scoreBoard[i] != 18):
+                anotherGo = 1
+        scoreBoard = setScoreBoard(maxScore[1],scoreBoard)
+        board = placeTile(board,bestLocation)
+        tileRack = removeTileFromRack(tileRack,bestLocation[2])
+        tileRack,tileBag = refreshTileRack(tileRack,tileBag)   
+    
+    return board,scoreBoard,tileRack,tileBag,gameOver,anotherGo
+
+
+
+
